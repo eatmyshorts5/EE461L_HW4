22c584b7f0b94a38c7cb68e984246ade77cc605f
Daniel Chen <daniel.chen@utexas.edu>
2018-04-04 17:38:42 -0500

    Test

--- app/src/main/java/com/example/daniel/ee461l_hw4/MainActivity.java
+++ app/src/main/java/com/example/daniel/ee461l_hw4/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.daniel.ee461l_hw4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText addressField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addressField = findViewById(R.id.address);
+
+        Button convertButton = findViewById(R.id.convert_button);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convert();
+            }
+        });
+
+    }
+
+    private void convert(){
+        
+
     }
 }
